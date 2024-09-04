5b712d4f79224dec9fcab3c42ca22c193089a3d8
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-04 15:48:12 -0300

    Atualizando `Aula04_Classes` com a funcao `extends`

--- FE-Introdução/Aula04_Classes/script.js
+++ FE-Introdução/Aula04_Classes/script.js
@@ -10,3 +10,18 @@ class pessoa {
 
 const pessoa1 = new pessoa("Maria", 25);
 pessoa1.saudacao();
+
+class Aluno extends pessoa {
+  constructor(nome, idade, anoEscolar) {
+    super(nome, idade);
+    this.anoEscolar = anoEscolar;
+  }
+  saudacao() {
+    console.log(
+      `Olá, me chamo ${this.nome} e tenho ${this.idade} anos. Estou no ${this.anoEscolar}`
+    );
+  }
+}
+
+const aluno = new Aluno("Carlos", 8, "3° ano");
+aluno.saudacao();
