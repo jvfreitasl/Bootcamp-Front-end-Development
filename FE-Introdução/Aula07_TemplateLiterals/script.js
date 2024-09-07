e31b59068c246a1735196a3ec458a1bca782b561
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-07 09:58:32 -0300

    Adicionando a pasta `Aula07_TemplateLiterals`

---
+++ FE-Introdução/Aula07_TemplateLiterals/script.js
@@ -0,0 +1,21 @@
+let nome = "Jota";
+let saudacao = `Olá meu nome é ${nome}`;
+
+console.log(saudacao);
+
+/////////////////
+
+let preco = 18;
+let quantidade = 4;
+let mensagemTotal = `O total a ser pago é ${preco * quantidade}`;
+
+/////////////////
+// É possível realizar quebra de linhas sem bugar o código
+
+const meuTexto = `Olá o meu nome é Jota
+
+eu sou o Dev desse programa
+
+Até Logo`;
+
+console.log(meuTexto);
