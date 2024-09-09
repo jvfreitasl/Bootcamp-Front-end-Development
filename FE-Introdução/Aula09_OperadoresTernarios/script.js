e6071964dd871e5995c1e8410b8c2b35baf08d53
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-09 11:28:15 -0300

    Adicionando a pasta `Aula09_OperadoresTernarios`

---
+++ FE-Introdução/Aula09_OperadoresTernarios/script.js
@@ -0,0 +1,10 @@
+const idade = 15;
+const podeDirigir = idade >= 18 ? "Sim" : "Não";
+
+console.log(`Posso dirigir? ${podeDirigir}`);
+
+const numero = 12;
+const resultado = numero % 2 === 0 ? "Par" : "Impar";
+
+console.log(`O número é ${resultado}`);
+console.log(`O número é ${numero % 2 === 0 ? "Par" : "Impar"}`);
